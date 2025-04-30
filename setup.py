3d894d8222f4e1c2197ee070528c68265a5545f4
Noah Sailer <57497859+NoahSailer@users.noreply.github.com>
2025-04-30 12:11:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/NoahSailer/MaPar",
     packages=setuptools.find_packages(),
-    package_data={'MaPar': extensions, 'MaPar/ckg_cgg': extensions},
+    package_data={'MaPar': extensions},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
