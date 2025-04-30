249c05607db811b598e68c05a4c8c3dca4a69667
Noah Sailer <57497859+NoahSailer@users.noreply.github.com>
2025-04-29 20:51:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/NoahSailer/MaPar",
-    packages=['MaPar'],
+    packages=setuptools.find_packages(),
     package_data={'MaPar': extensions},
     classifiers=[
         "Programming Language :: Python :: 3",
